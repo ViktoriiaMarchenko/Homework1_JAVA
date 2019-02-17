759a298ffc8791966ef029e29c86ed6fc6dc085f
Viktoriia Marchenko <webcolorm@gmail.com>
2019-02-17 23:57:32 +0200

    JAVA
    JAVA Homework 1

---
+++ DZ1.java
@@ -0,0 +1,12 @@
+package com.groupqa1.test;
+
+public class DZ1 {
+
+    public static void main (String[] args){
+        System.out.println("**** ****     *     *     *");
+        System.out.println("*    *  *    * *    * * * *");
+        System.out.println("**   ****   *   *   *  *  *");
+        System.out.println("*    *     * *** *  *     *");
+        System.out.println("**** *    *       * *     *");
+    }
+}
